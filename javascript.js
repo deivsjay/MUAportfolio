53e0da76849a03420f93f24472b7e427dd43be56
Divya Jagadeesh <djagadee@usc.edu>
2018-09-11 08:15:43 -0700

    social media added to footer

--- javascript.js
+++ javascript.js
@@ -3,4 +3,13 @@ $(document).on('click','#enter',function(e) {
     $("#halfsParent").hide();
     $("body").attr("style", "background-color: white !important;");
     $("#portfolio").show();
+    animatePictures();
 });
+
+function animatePictures() {
+    $("#portfolio-footer").delay(1000).show(0);
+   jQuery('.portfolio-picture').each(function(i) {
+      jQuery(this).delay(i * 500).fadeIn(5000);
+    });
+}
+//jQuery(window).load(function($) { animatePictures(); });
