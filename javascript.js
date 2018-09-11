322b61876d9c28657ff69cd25d68148cc5406323
Divya Jagadeesh <djagadee@usc.edu>
2018-09-11 01:24:01 -0700

    need to add footer to desktop design, need to make mobile design

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// enter button
+$(document).on('click','#enter',function(e) {
+    $("#halfsParent").hide();
+    $("body").attr("style", "background-color: white !important;");
+    $("#portfolio").show();
+});
